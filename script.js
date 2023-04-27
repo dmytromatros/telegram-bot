f9e80e6a72e862310144df25a0ad77d97899865b
dev2 <andrii@asdf.de>
2023-04-27 13:04:20 +0300

    some fixs

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ tg.MainButton.onClick(() => {
 
 tg.MainButton.setText('Do something');
 tg.BackButton.isVisible = true;
+tg.onEvent('mainButtonClicked', () => {
+    console.log('Hello Main button by tg!')
+})
+
+document.querySelectorAll('.button').forEach(e => {
+    e.addEventListener('click', () => {
+        console.log("just a button clicked!")
+    })
+})
